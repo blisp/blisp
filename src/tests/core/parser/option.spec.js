c0e28fee5c3decbeca6048b4f8245cebfee6f37b
Sean Lynch <sean@seanplynch.com>
2018-10-28 14:01:44 -0700

    Add eslint fix option to call eok instead of error

--- src/tests/core/parser/option.spec.js
+++ src/tests/core/parser/option.spec.js
@@ -4,9 +4,19 @@ function consumeError(input, pos, _, cerr) {
   return cerr("foo", pos + 1, input)
 }
 
+function emptyError(input, pos, _, cerr) {
+  return cerr("foo", pos, input)
+}
+
 describeModule("@tbdscript/core/parser/option", (option) => {
   describeCombinator(option, consumeError, (parser) => {
-    describeTest(parser, "abc", 0, "eerr", (result, pos) => {
+    describeTest(parser, "abc", 0, "eok", (result, pos) => {
+      itReadsString("foo", (str) => equal(result, str))
+      itEndsAtPosition(0, (end) => equal(pos, end))
+    })
+  })
+  describeCombinator(option, emptyError, (parser) => {
+    describeTest(parser, "abc", 0, "eok", (result, pos) => {
       itReadsString("foo", (str) => equal(result, str))
       itEndsAtPosition(0, (end) => equal(pos, end))
     })
