66d5c652d2e98621a0752edd2c480524ce57935a
Sean Lynch <sean@seanplynch.com>
2019-01-01 17:55:46 -0800

    Add quote and syntaxQuote special forms

--- src/tests/expander/expand.spec.js
+++ src/tests/expander/expand.spec.js
@@ -2,9 +2,14 @@ require("@blisp/reader/read")
 const { expect } = require("chai")
 const {
   arrayExpression,
+  objectProperty,
+  objectExpression,
+  stringLiteral,
+  numericLiteral,
+  booleanLiteral,
+  nullLiteral,
   callExpression,
   identifier,
-  stringLiteral,
   memberExpression,
 } = require("@babel/types")
 const syntax = require("@blisp/reader/syntax")
@@ -80,5 +85,110 @@ describeModule("@blisp/expander/expand", (expand) => {
         )
       )
     })
+  })
+  describe("quote", () => {
+    const number = syntax("", 1)
+    describeSyntax(
+      syntax("", [syntax("", Symbol.for("quote")), number]),
+      (syntax) => {
+        it("it stops expansion", () => {
+          expect(expand(syntax, env)).to.equal(number)
+        })
+      }
+    )
+  })
+  describe("syntaxQuote", () => {
+    describeSyntax(
+      syntax("", [syntax("", Symbol.for("syntaxQuote")), numericLiteral(1)]),
+      (syntax) => {
+        it("it expands to babel object expression", () => {
+          expect(expand(syntax, env)).to.eql(
+            objectExpression([
+              objectProperty(
+                identifier("type"),
+                stringLiteral("NumericLiteral")
+              ),
+              objectProperty(identifier("value"), numericLiteral(1)),
+            ])
+          )
+        })
+      }
+    )
+    describeSyntax(
+      syntax("", [syntax("", Symbol.for("syntaxQuote")), stringLiteral("foo")]),
+      (syntax) => {
+        it("it expands to babel object expression", () => {
+          expect(expand(syntax, env)).to.eql(
+            objectExpression([
+              objectProperty(
+                identifier("type"),
+                stringLiteral("StringLiteral")
+              ),
+              objectProperty(identifier("value"), stringLiteral("foo")),
+            ])
+          )
+        })
+      }
+    )
+    describeSyntax(
+      syntax("", [syntax("", Symbol.for("syntaxQuote")), booleanLiteral(true)]),
+      (syntax) => {
+        it("it expands to babel object expression", () => {
+          expect(expand(syntax, env)).to.eql(
+            objectExpression([
+              objectProperty(
+                identifier("type"),
+                stringLiteral("BooleanLiteral")
+              ),
+              objectProperty(identifier("value"), booleanLiteral(true)),
+            ])
+          )
+        })
+      }
+    )
+    describeSyntax(
+      syntax("", [
+        syntax("", Symbol.for("syntaxQuote")),
+        booleanLiteral(false),
+      ]),
+      (syntax) => {
+        it("it expands to babel object expression", () => {
+          expect(expand(syntax, env)).to.eql(
+            objectExpression([
+              objectProperty(
+                identifier("type"),
+                stringLiteral("BooleanLiteral")
+              ),
+              objectProperty(identifier("value"), booleanLiteral(false)),
+            ])
+          )
+        })
+      }
+    )
+    describeSyntax(
+      syntax("", [
+        syntax("", Symbol.for("syntaxQuote")),
+        syntax("", Symbol.for("foo")),
+      ]),
+      (syntax) => {
+        it("it expands to babel object expression", () => {
+          expect(expand(syntax, env)).to.eql(
+            objectExpression([
+              objectProperty(
+                identifier("type"),
+                stringLiteral("SymbolLiteral")
+              ),
+              objectProperty(
+                identifier("value"),
+                callExpression(
+                  memberExpression(identifier("Symbol"), identifier("for")),
+                  [stringLiteral("foo")]
+                )
+              ),
+            ])
+          )
+        })
+      }
+    )
   })
 })
